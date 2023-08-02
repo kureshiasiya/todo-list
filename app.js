ace335aca42e175c29f0413e8e6a9a4eacf712fe
kureshiasiya <kureshiasiya2@gmail.com>
2023-08-02 16:05:47 +0530

    buttons in process

---
+++ app.js
@@ -0,0 +1,36 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+//Event Listeners
+
+todoButton.addEventListener("click,addtodo");
+//Functions
+
+function addTodo(event) {
+  //prevent form from submitting
+  event.preventDefault();
+  //Todo DIV
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //creat LI
+
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //CHECK MARK BUTTON
+
+  const completedButton = document.createElement("button");
+  compeletedButton.innerText = '<class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+  //CHECK TRASH BUTTON
+
+  const trashButton = document.createElement("button");
+  trashButtonutton.innerText = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("complete-btn");
+  todoDiv.appendChild(trashButton);
+  //APPEND TO LIST
+  todoList.appendChild(todoDiv);
+}
